14bd7ca2c058252280af9b9df5bf4c3ace8c2c95
civin <civin@bupt.edu.cn>
2019-12-23 23:59:14 +0800

    update

--- scds.bat
+++ scds.bat
@@ -1,3 +1,2 @@
-echo '开始进行部署blog到gitee'
-git checkout gitee && git merge master && git push && hexo g && hexo d && git checkout master
-echo '批处理结束'
+echo '开始部署到Github'
+git push && hexo g && hexo d && echo '开始进行部署blog到gitee' && git checkout gitee && git merge master && git push && hexo g && hexo d && git checkout master && echo '批处理结束'
---
+++ source/_drafts/Hexo系列.md
@@ -0,0 +1,22 @@
+---
+title: Hexo系列
+date: 2019-12-23 23:53:58
+tags:
+- tag
+categories: Hexo系列
+mathjax:
+---
+摘要
+>
+
+<!--more-->
+
+Hexo中的坑
+
+Latex公式显示问题
+Latex公式换行问题
+Gitalk,disqus,liv2d,
+post文件命名问题
+文章摘要问题
+Gitee部署问题
+标签，目录， 文章字数计数，站内搜索
