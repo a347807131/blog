5113c1c03c182c1635d8a4b052177aea8625e89d
civin <civin@bupt.edu.cn>
2019-12-25 21:54:41 +0800

    update git clone <repository> --recursive #¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿? git submodule add <repository> <path> #¿?¿?¿?¿?¿? git submodule init #¿?¿?¿?¿?¿?¿? git submodule update #¿?¿?¿?¿?¿? git submodule foreach git pull #¿?¿?¿?¿?¿?¿?¿?

--- scds.bat
+++ scds.bat
@@ -1,3 +1,2 @@
-
 echo '开始部署到Github'
 git push  && git checkout master && git merge dev && git push && hexo g -d && echo '开始进行部署blog到gitee' && git checkout gitee && git merge master && git push && hexo g -d && git checkout master && echo '批处理结束'
--- source/_drafts/Hexo系列.md
+++ source/_drafts/Hexo系列.md
@@ -17,10 +17,15 @@ Hexo中的坑
 
 Latex公式显示问题
 Latex公式换行问题
+valine邮件通知
+163music自动播放
 Gitalk,disqus,liv2d,valine,addthis,music
 post文件命名问题
 文章摘要问题
 Gitee部署问题
 标签，目录， 文章字数计数，站内搜索
 algolia踩坑
+七牛云
+图片居中、缩放问题
+
 back2top进度
